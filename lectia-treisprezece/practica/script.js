80344bbdbc457a412722b7e7e1ef282d96efe769
smoothwizz <a.popa@goit.global>
2023-08-14 21:04:23 +0300

    Adaugare lectia 13

---
+++ lectia-treisprezece/practica/script.js
@@ -0,0 +1,65 @@
+// EXERCISE 1
+
+// const sectionEl = document.querySelector(".section-exercise");
+// const divEl = document.querySelector(".section-exercise div");
+// const paragraphEl = document.querySelector(".section-exercise p");
+
+// document.addEventListener("click", () => {
+//   console.log("Click pe document");
+// });
+
+// sectionEl.addEventListener("click", (e) => {
+//   e.stopPropagation();
+//   console.log("Ai apasat pe sectiune.");
+// });
+
+// divEl.addEventListener("click", () => {
+//   console.log("Ai apasat pe div.");
+// });
+
+// paragraphEl.addEventListener("click", () => {
+//   console.log("Ai apasat pe paragraf.");
+// });
+
+// EXERCISE 2
+const data = {
+  quantity: 0,
+  price: 0,
+  calcTotalPrice() {
+    return (this.quantity * this.price).toFixed(2);
+  },
+};
+
+const formEl = document.querySelector("#form");
+const priceEl = document.querySelector("#price");
+const quantityEl = document.querySelector("#quantity");
+const amountDisplayEl = document.querySelector("#amount");
+const totalDisplayEl = document.querySelector("#total");
+const labelEl = document.querySelector(".form label:last-of-type");
+
+console.log(labelEl);
+
+formEl.addEventListener("change", (e) => {
+  if (e.target === priceEl) {
+    console.log(
+      "Pret modificat " +
+        priceEl.value +
+        " pentru produsul si cantitatea " +
+        quantityEl.value
+    );
+
+    console.log(
+      `Pret modificat ${priceEl.value} pentru produsul si cantitatea ${quantityEl.value}`
+    );
+
+    data.price = priceEl.value;
+  }
+
+  if (e.target === quantityEl) {
+    console.log("Cantitate modificata");
+    data.quantity = quantityEl.value;
+  }
+
+  amountDisplayEl.textContent = quantityEl.value;
+  totalDisplayEl.textContent = data.calcTotalPrice();
+});
---
+++ lectia-treisprezece/practica/tasks.js
@@ -0,0 +1,85 @@
+// Emergence of events
+// const parentDiv = document.getElementById("parentDiv");
+// const childA = document.getElementById("childA");
+
+// document.body.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     alert("body");
+// });
+
+// parentDiv.addEventListener("click", (e) => {
+//     alert("Parent Div");
+// });
+
+// childA.addEventListener("click", (e) => {
+//     e.stopPropagation();
+//     e.preventDefault();
+//     alert("Child A");
+// });
+
+//! ===================================================
+
+// for (let elem of document.querySelectorAll("*")) {
+//   elem.addEventListener("click", (e) => alert(`Diving: ${elem.tagName}`), true);
+//   elem.addEventListener("click", (e) => alert(`Emersion: ${elem.tagName}`));
+// }
+
+//!=============================================
+
+// Create a product cost calculator depending on the price. When working with events, use delegation. Render all markup via JS
+// When the data variable is entered in input #1 or #2, the total cost should be automatically recalculated
+// The currency - USD should always be displayed
+// The total amount is displayed with pennies (2 decimal places)
+// In the signature of the second input, there should be a color hint of how many pounds the user selected on the second input.
+// The color tooltip should change its value when the slider is dragged.
+// In input No. 2, set min and max threshold by yourself.
+// When loading the page, the script should automatically calculate the cost based on the default data that you set in the markup.
+
+const data = {
+  quantity: 0,
+  price: 0,
+  calcTotalPrice() {
+    return (this.quantity * this.price).toFixed(2);
+  },
+};
+
+//dom links
+const mainForm = document.getElementById("form");
+const priceInput = document.getElementById("price");
+const quantityInput = document.getElementById("quantity");
+const amount = document.getElementById("amount");
+const total = document.getElementById("total");
+
+//data fill
+dataFill();
+displayTotal();
+
+//events
+mainForm.addEventListener("change", handleFormChange);
+
+//functions
+function handleFormChange({ target }) {
+  const { value } = target;
+
+  if (target === priceInput) {
+    if (!Number(value)) {
+      console.log("Not a number!");
+      return;
+    }
+  } else if (target === quantityInput) {
+    amount.textContent = value;
+  }
+
+  target.setAttribute("value", value);
+  dataFill();
+  displayTotal();
+}
+
+function dataFill() {
+  data.quantity = quantityInput.getAttribute("value");
+  data.price = priceInput.getAttribute("value");
+}
+
+function displayTotal() {
+  total.textContent = data.calcTotalPrice() + " USD";
+}
